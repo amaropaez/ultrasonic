ec3a3577ff303f0560c4522679c40078b64fd079
Nadia Amaro <amaropaez@gmail.com>
2023-03-09 15:00:58 -0300

    guardar cabios

--- js/main.js
+++ js/main.js
@@ -100,12 +100,15 @@ console.log('el total a pagar por su compra es : ${total}')
 
 /****carrito******/
 
+const shopContent = document.getElementById("shopContent")
+
+
 const productos = [
   {
     id: 11
     nombre: "panel acustico 50x50x3cm",
     precio: 410,
-    img: "",
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_865529-MLA43938604533_102020-F.webp",
 
   },
 
@@ -113,56 +116,72 @@ const productos = [
     id: 2,
     nombre: "panel acustico 100x50x3cm",
     precio: 820,
-    img: "",
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_865529-MLA43938604533_102020-F.webp",
   },
 
   {
     id: 3,
     nombre: "panel acustico 50x50x3.5cm",
     precio: 524.50,
-    img: "",
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_865529-MLA43938604533_102020-F.webp",
   },
   {
     id: 4,
     nombre: "panel acustico 100x50x3.5cm",
     precio: 1049,
-    img: "",
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_865529-MLA43938604533_102020-F.webp",
   },
   {
     id: 5,
     nombre: "panel acustico 50x50x5cm",
     precio: 698.50,
-    img: "",
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_865529-MLA43938604533_102020-F.webp",
   },
   {
     id: 6,
     nombre: "panel acustico 100x50x5cm",
     precio: 1397,
-    img: "",
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_865529-MLA43938604533_102020-F.webp",
   },
-  id: 7,
+
   {
+    id: 7
     nombre: "panel acustico 50x50x7cm",
     precio: 995.75,
-    img: "",
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_865529-MLA43938604533_102020-F.webp",
   },
   {
     id: 8,
     nombre: "panel acustico 100x50x7cm",
     precio: 1991.50,
-    img: "",
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_865529-MLA43938604533_102020-F.webp",
   },
   {
     id: 9,
     nombre: "panel acustico 50x50x7.5cm",
     precio: 1009.50,
-    img: "",
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_865529-MLA43938604533_102020-F.webp",
   },
   {
     id: 10,
     nombre: "panel acustico 100x50x7.5cm",
     precio: 2018.50,
-    img: "",
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_865529-MLA43938604533_102020-F.webp",
   },
-  
-]
+
+];
+
+let carrito = [];
+
+  productos.forEach((product) => {
+let content = document.createElement("div");
+content.innerHTML = '
+<img src="${product.img}">
+<h3> ${product.nombre} </h3>
+<p> ${product.precio} $</p>
+';
+
+
+shopContent.append(content)
+
+});
