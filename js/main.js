499a598c2928c5943e18739694dbb2b9949a2fb3
Nadia Amaro <amaropaez@gmail.com>
2023-03-10 10:47:41 -0300

    guardar cambios en carrito se agrego poder eliminar items en el carrito

--- js/main.js
+++ js/main.js
@@ -107,7 +107,7 @@ productos.forEach((product) => {
 
 const pintarCarrito = () => {
   modalContainer.innerHTML = "";
-modalContainer.style.display = "flex"
+  modalContainer.style.display = "flex"
 
   const modalHeader = document.createElement("div");
   modalHeader.className = "modal-header"
@@ -139,25 +139,36 @@ modalContainer.style.display = "flex"
     modalContainer.append(carritoContent);
 
 
-let eliminar = document.createElement("span");
-eliminar.innerText = "❎";
-eliminar.className = "delete-product";
-carritoContent.append(eliminar);
+    let eliminar = document.createElement("span");
+    eliminar.innerText = "❎";
+    eliminar.className = "delete-product";
+    carritoContent.append(eliminar);
 
+    eliminar.addEventListener("click", eliminarProducto);
   });
 
 
 
 
-const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+  const total = carrito.reduce((acc, el) => acc + el.precio, 0);
 
-const totalBuying = document.createElement("div")
-totalBuying.className = "total-content"
-totalBuying.innerHTML = `total a pagar: ${total} $`;
-modalContainer.append(totalBuying);
+  const totalBuying = document.createElement("div")
+  totalBuying.className = "total-content"
+  totalBuying.innerHTML = `total a pagar: ${total} $`;
+  modalContainer.append(totalBuying);
 
 
 };
 
 verCarrito.addEventListener("click", pintarCarrito);
 
+const eliminarProducto = () => {
+  const foundId = carrito.find((element) => element.id);
+
+  carrito = carrito.filter((carritoId) => {
+    return carritoId !== foundId;
+  });
+
+
+  pintarCarrito();
+};
